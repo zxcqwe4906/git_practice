2482408eea1df0031eb621f7ccd60c30806c9bbe
Jack <zxcqwe490649064906@gmail.com>
2023-01-05 17:02:14 +0800

    Let user add three numbers
    modify `add` function to let user add two or three numbers

--- simple_calculate_class.py
+++ simple_calculate_class.py
@@ -1,8 +1,13 @@
 class Calculate:
+<<<<<<< HEAD
     def add(self, a: int, b: int):
         if not (isinstance(a, int) and isinstance(b, int)):
             raise TypeError('Input must be int.')
         return a + b
+=======
+    def add(self, a, b, c=0):
+        return a + b + c
+>>>>>>> feb0ff9 (Let user add three numbers)
 
     def sub(self, a: int, b: int):
         if not (isinstance(a, int) and isinstance(b, int)):
@@ -13,3 +18,4 @@ if __name__ == '__main__':
     c = Calculate()
     print(f'5 + 8 = {c.add(5, 8)}')
     print(f'5 - 8 = {c.sub(5, 8)}')
+    print(f'5 + 8 + 9 = {c.add(5, 8, 9)}')
