08f5c95dda51fcdabcce4a6da708ca053be9d69d
JamieKeng <>
2023-01-16 17:50:40 +0800

    Solve [issue #1] on add function
    No comment on this one.

--- simple_calculate_class.py
+++ simple_calculate_class.py
@@ -1,12 +1,21 @@
+import numpy as np
+
 class Calculate:
-    def add(self, a, b, c=0):
-        return a + b + c
+    def add(self, *nums):
+        for num in nums:
+            if not isinstance(num, int):
+                print('Only integer inputs, please.')
+                return 
+
+        return np.sum(nums)
+
 
     def sub(self, a, b):
         return a - b
 
 if __name__ == '__main__':
     c = Calculate()
-    print(f'5 + 8 = {c.add(5, 8)}')
-    print(f'5 - 8 = {c.sub(5, 8)}')
-    print(f'5 + 8 + 9 = {c.add(5, 8, 9)}')
+    print(c.add((5, 7, 9, 10, 11, 50, 34, 3.3)))
+    #print(f'5 + 8 = {c.add(5, 8, 7, 3, 10)}')
+    #print(f'5 - 8 = {c.sub(5, 8)}')
+    #print(f'5 + 8 + 9 = {c.add(5, 8, 9)}')
