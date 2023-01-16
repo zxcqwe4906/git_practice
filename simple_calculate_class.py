a960ee601992cad7953571c6728b267bfb393345
JamieKeng <>
2023-01-16 18:14:32 +0800

    Black formation and isort
    No comment on this one.

--- simple_calculate_class.py
+++ simple_calculate_class.py
@@ -1,22 +1,24 @@
 import numpy as np
 
+
 class Calculate:
     def add(self, *nums):
         for num in nums:
             if not isinstance(num, int):
-                raise TypeError('Inputs must be integers')
-                return 
+                raise TypeError("Inputs must be integers")
+                return
 
         return np.sum(nums)
 
     def sub(self, a: int, b: int):
         if not (isinstance(a, int) and isinstance(b, int)):
-            raise TypeError('Input must be int.')
+            raise TypeError("Input must be int.")
         return a - b
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     c = Calculate()
     print(c.add((5, 7, 9, 10, 11, 50, 34, 3.3)))
-    #print(f'5 + 8 = {c.add(5, 8, 7, 3, 10)}')
-    #print(f'5 - 8 = {c.sub(5, 8)}')
-    #print(f'5 + 8 + 9 = {c.add(5, 8, 9)}')
+    # print(f'5 + 8 = {c.add(5, 8, 7, 3, 10)}')
+    # print(f'5 - 8 = {c.sub(5, 8)}')
+    # print(f'5 + 8 + 9 = {c.add(5, 8, 9)}')
