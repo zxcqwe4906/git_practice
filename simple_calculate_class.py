cc8b90b6ddefce18f4a94ad36edc2ee8b66b9607
JamieKeng <>
2023-01-18 10:37:20 +0800

    Solve Problems described in issue [#5]
    Please seee issue [#5] on Github.

--- simple_calculate_class.py
+++ simple_calculate_class.py
@@ -2,13 +2,12 @@ import numpy as np
 
 
 class Calculate:
-    def add(self, *nums):
+    def add(self, *nums: int):
         for num in nums:
             if not isinstance(num, int):
                 raise TypeError("Inputs must be integers")
-                return
 
-        return np.sum(nums)
+        return int(np.sum(nums))
 
     def sub(self, a: int, b: int):
         if not (isinstance(a, int) and isinstance(b, int)):
@@ -18,7 +17,5 @@ class Calculate:
 
 if __name__ == "__main__":
     c = Calculate()
-    print(c.add((5, 7, 9, 10, 11, 50, 34, 3.3)))
-    # print(f'5 + 8 = {c.add(5, 8, 7, 3, 10)}')
-    # print(f'5 - 8 = {c.sub(5, 8)}')
-    # print(f'5 + 8 + 9 = {c.add(5, 8, 9)}')
+    #print(c.add((5, 7, 9, 10, 11, 50, 34, 3.3)))
+    print(type(c.add()))
